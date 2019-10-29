983c3d5c640b1ea4efae6a8e50fe467a8ac5fcc4
papaIbraDiagne <papaibra1998@gmail.com>
2019-10-29 22:38:21 +0000

    29/10 22:39

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * 
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+    
+        return $this->render("home.html.twig");
+    }
+
+    
+}
